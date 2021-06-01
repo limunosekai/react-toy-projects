412f04b6463c3ab06085253a55e237f539517a95
limunosekai <powerlsh0103@gmail.com>
2021-06-01 19:01:56 +0900

    Add Tour project && Render Cards

--- src/Tours/Tours.js
+++ src/Tours/Tours.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Loading from './Loading';
+import List from './List';
+import './index.css';
+
+const url = 'https://course-api.com/react-tours-project';
 
 function Tours() {
-  return <div></div>;
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const toursData = await response.json();
+      setLoading(false);
+      setTours(toursData);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <List tours={tours} />
+    </main>
+  );
 }
 
 export default Tours;
