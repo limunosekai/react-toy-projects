24a4f7839138fdd3f7bb6275cc3349f48d908ca6
limunosekai <powerlsh0103@gmail.com>
2021-06-09 16:18:11 +0900

    End Color Generator Project

--- src/ColorGenerator/Color.js
+++ src/ColorGenerator/Color.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import rgbToHex from './util';
 
-function Color() {
-  return <div></div>;
+function Color({ rgb, weight, index, hex }) {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
+
+  // rgb -> hex Util
+  // const hexColor = rgbToHex(...rgb);
+
+  const hexValue = `#${hex}`;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        navigator.clipboard.writeText(hexValue);
+        setAlert(true);
+      }}
+    >
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexValue}</p>
+      {alert && <p className='alert'>Copied to Clipboard!</p>}
+    </article>
+  );
 }
 
 export default Color;
--- src/ColorGenerator/ColorGenerator.js
+++ src/ColorGenerator/ColorGenerator.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './color.css';
 import Color from './Color';
+import Values from 'values.js';
 
 function ColorGenerator() {
-  return <div></div>;
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#f15025').all(5));
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(5);
+      setList(colors);
+      setError(false);
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  return (
+    <>
+      <section className='color-section color-container'>
+        <h3>Color Generator</h3>
+        <form onSubmit={submitHandler}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.currentTarget.value)}
+            placeholder='ex) #f15025'
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='color-btn' type='submit'>
+            Submit
+          </button>
+        </form>
+      </section>
+      {error ? (
+        <h4 className='error-text center'>Please check again!</h4>
+      ) : (
+        <h2 className='center'>Color List</h2>
+      )}
+      <section className='color-section colors'>
+        {list.map((item, index) => {
+          return <Color key={index} {...item} hex={item.hex} index={index} />;
+        })}
+      </section>
+    </>
+  );
 }
 
 export default ColorGenerator;
