09905fff54284018768051c2183567f14284af32
limunosekai <powerlsh0103@gmail.com>
2021-06-20 18:30:35 +0900

    Complete Markdown Project

--- src/Markdown/Markdown.js
+++ src/Markdown/Markdown.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './markdown.css';
+import ReactMarkdown from 'react-markdown';
 
 function Markdown() {
-  return <div></div>;
+  const [markdown, setMarkdown] = useState('### Markdown Preview 🍄');
+  return (
+    <main className='markdown-section'>
+      <section className='markdown'>
+        <textarea
+          className='markdown-input'
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className='markdown-result'>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  );
 }
 
 export default Markdown;
