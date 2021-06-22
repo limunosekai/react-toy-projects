378431e673e809e5df71d19e2924784effcf1bc4
limunosekai <powerlsh0103@gmail.com>
2021-06-22 15:56:35 +0900

    Add Search Form

--- src/HackerNews/SearchForm.js
+++ src/HackerNews/SearchForm.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import { useGlobalContext } from './context';
+
 function SearchForm() {
-  return <div>dd</div>;
+  const { searchHandler, query } = useGlobalContext();
+  return (
+    <form className='hacker-search-form' onSubmit={(e) => e.preventDefault()}>
+      <h2>Search Hacker News!</h2>
+      <input
+        type='text'
+        className='hacker-form-input'
+        value={query}
+        onChange={(e) => searchHandler(e.target.value)}
+      />
+    </form>
+  );
 }
 
 export default SearchForm;
