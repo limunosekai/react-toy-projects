de7a0ae10b8236dbcc095654cc1c7521a3dcd69a
limunosekai <powerlsh0103@gmail.com>
2021-06-22 14:05:58 +0900

    Add Dark Mode Toggle Btn Func

--- src/Dark/Dark.js
+++ src/Dark/Dark.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './dark.css';
 import Article from './Article';
 import data from './data';
 
 function Dark() {
+  const [theme, setTheme] = useState('light-theme');
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+
   return (
     <div>
-      <nav>
+      <nav className='dark-nav'>
         <div className='dark-nav-center'>
           <h1>Dark Mode</h1>
-          <button className='dark-btn'>Toggle</button>
+          <button className='dark-btn' onClick={toggleTheme}>
+            Toggle
+          </button>
         </div>
       </nav>
       <div className='dark-articles'>
