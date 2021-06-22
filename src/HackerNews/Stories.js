b39001731bce2453a8b8bb7bbd31a6c7d8e1fee2
limunosekai <powerlsh0103@gmail.com>
2021-06-22 15:25:07 +0900

    Display Fetch Items

--- src/HackerNews/Stories.js
+++ src/HackerNews/Stories.js
@@ -3,12 +3,38 @@ import React from 'react';
 import { useGlobalContext } from './context';
 
 function Stories() {
-  const { isLoading } = useGlobalContext();
+  const { isLoading, hits } = useGlobalContext();
 
   if (isLoading) {
     return <div className='hacker-loading'></div>;
   }
-  return <div>dd</div>;
+  return (
+    <section className='hacker-stories'>
+      {hits.map((item) => {
+        const { objectID, title, num_comments, url, points, author } = item;
+        return (
+          <article key={objectID} className='hacker-story'>
+            <h4 className='hacker-title'>{title}</h4>
+            <p className='hacker-info'>
+              {points} points by <span>{author} | </span>
+              {num_comments} comments
+            </p>
+            <div>
+              <a
+                target='_blank'
+                href={url}
+                className='hacker-read-link'
+                rel='noreferrer'
+              >
+                Read More
+              </a>
+              <button className='hacker-remove-btn'>Remove</button>
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
 }
 
 export default Stories;
