98d54595c7b6c4a6533aca0a4bf93aa6640f619d
limunosekai <powerlsh0103@gmail.com>
2021-06-23 12:19:15 +0900

    Init Quiz Project

---
+++ src/Quiz/Quiz.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useGlobalContext } from './context';
+import SetupForm from './SetupForm';
+import Loading from './Loading';
+import Modal from './Modal';
+
+function Quiz() {
+  return <div></div>;
+}
+
+export default Quiz;
